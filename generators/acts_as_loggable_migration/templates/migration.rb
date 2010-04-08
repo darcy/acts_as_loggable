1fff993748cf0a2c9bd31aad16241a5175fd40fd
Edward Tsech <edtsech@gmail.com>
2010-04-09 00:25:29 +0700

    Refactored migration

--- generators/acts_as_loggable_migration/templates/migration.rb
+++ generators/acts_as_loggable_migration/templates/migration.rb
@@ -1,21 +1,20 @@
 class ActsAsLoggableMigration < ActiveRecord::Migration
   def self.up
-    create_table "logs" do |t|
-      t.string "message"
-      t.integer "loggable_id"
-      t.string "loggable_type"
-      t.integer "user_id"
-      t.string "group"
-      t.integer "owner_id"
-      t.string "owner_type"
+    create_table :logs do |t|
+      t.string :message
+      t.references :loggable, :polymorphic => true
+      t.references :user
+      t.string :group
+      t.references :owner,  :polymorphic => true
       t.timestamps
     end
-    add_index "logs", ["loggable_id","loggable_type"]
     
-    create_table "log_details" do |t|
-      t.integer "log_id"
-      t.string "label"
-      t.text "detail"
+    add_index :logs, [:loggable_id, :loggable_type]
+    
+    create_table :log_details do |t|
+      t.references :log
+      t.string :label
+      t.text :detail
     end
   end
 
